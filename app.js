619f45dacd400d68ce87ca482a09cfd7eaeb9c80
Anatoliy Sannikov <WildJuk@yandex.ru>
2023-07-09 13:32:12 +0500

    feat: ПР №14 сервер на express

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const UnknowErr = require('./middlewares/unknow-err');
 const router = require('./routes');
@@ -15,7 +14,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(router);
 app.use(errors());
 app.use(UnknowErr);
